f02fb6ea05c4b716142933d49c6b6f118cb6c04f
Manihasreddy <66477892+Manihasreddy@users.noreply.github.com>
2020-06-06 21:20:53 +0530

    Add files via upload

---
+++ A1.py
@@ -0,0 +1,13 @@
+list=[1,2,3,4]
+list1=["apple","samsung","black berry"]
+list.append(5)
+list1.append("intex")
+print(list)
+print(list1)
+tuple1=('english','hindi','malayalam','kannada','orissa','tamil')
+print(tuple1[0])
+print(tuple1[2])
+print(tuple1[4])
+dictionary = {'apple':1,'ball':2,'cat':3,'dog':4,'egg':5}
+del dictionary['cat']
+print(dictionary)
